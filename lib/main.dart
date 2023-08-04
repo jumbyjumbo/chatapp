979706db378a722e004bd7bc3079eac6639e9eba
zaksorel <sorelzakary@gmail.com>
2023-08-04 15:36:54 -0400

    new logo

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'backend stuff/firebase_options.dart';
 import 'backend stuff/authservice.dart';
 import 'pages/login.dart';
