ceb80d5b33537cebdb4a5ab340366cb2f5de1d7a
zaksorel <sorelzakary@gmail.com>
2023-08-16 12:52:28 -0400

    online marking fully implmented

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -21,10 +23,56 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // Create a navigator key to navigate without context
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
+  final AuthService authService = AuthService(FirebaseAuth.instance);
+  Timer? inactivityTimer;
+
+  //user activity detected, reset inactivity timer
+  void userActivityDetected() {
+    if (inactivityTimer != null) {
+      inactivityTimer!.cancel();
+    }
+    final currentUser = FirebaseAuth.instance.currentUser;
+    if (currentUser != null) {
+      authService.markUserOnline(currentUser.uid);
+    }
+    inactivityTimer = Timer(const Duration(minutes: 10), () {
+      final currentUser = FirebaseAuth.instance.currentUser;
+      if (currentUser != null) {
+        authService.markUserOffline(currentUser.uid);
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    inactivityTimer?.cancel();
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    final currentUser = FirebaseAuth.instance.currentUser;
+    if (state == AppLifecycleState.paused && currentUser != null) {
+      authService.markUserOffline(currentUser.uid);
+    } else if (state == AppLifecycleState.resumed && currentUser != null) {
+      userActivityDetected();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,61 +81,68 @@ class MyApp extends StatelessWidget {
       FirebaseAuth.instance,
     );
 
-    return CupertinoApp(
-      //app title
-      title: "Flow",
+    //wrap app in gesture detector to detect user activity
+    return GestureDetector(
+      onTap: userActivityDetected,
 
-      // Set theme.
-      theme: CupertinoThemeData(
-        primaryColor: CupertinoTheme.brightnessOf(context) == Brightness.light
-            ? CupertinoColors.black
-            : CupertinoColors.white,
-      ),
+      //actual app widget
+      child: CupertinoApp(
+        //app title
+        title: "Flow",
 
-      //stream if user is logged in or not
-      home: StreamBuilder<User?>(
-        stream: authService.authStateChanges,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.active) {
-            if (snapshot.data == null) {
-              // User is signed out, show Login screen.
-              return const Login();
-            } else {
-              // Get user document
-              final userDoc = FirebaseFirestore.instance
-                  .collection('users')
-                  .doc(snapshot.data!.uid);
+        // Set theme.
+        theme: CupertinoThemeData(
+          primaryColor: CupertinoTheme.brightnessOf(context) == Brightness.light
+              ? CupertinoColors.black
+              : CupertinoColors.white,
+        ),
 
-              // Check if user has a username
-              return FutureBuilder<DocumentSnapshot>(
-                  future: userDoc.get(),
-                  builder: (context, userInfo) {
-                    if (userInfo.hasData) {
-                      final userData =
-                          userInfo.data!.data() as Map<String, dynamic>?;
+        //stream if user is logged in or not
+        home: StreamBuilder<User?>(
+          stream: authService.authStateChanges,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.active) {
+              if (snapshot.data == null) {
+                // User is signed out, show Login screen.
+                return const Login();
+              } else {
+                // Get user document
+                final userDoc = FirebaseFirestore.instance
+                    .collection('users')
+                    .doc(snapshot.data!.uid);
 
-                      //if user has no username, go to username selection page
-                      if (userData!['username'] == null) {
-                        // Navigate to username selection page
-                        return const UsernameSelection();
+                // Check if user has a username
+                return FutureBuilder<DocumentSnapshot>(
+                    future: userDoc.get(),
+                    builder: (context, userInfo) {
+                      if (userInfo.hasData) {
+                        final userData =
+                            userInfo.data!.data() as Map<String, dynamic>?;
+
+                        //if user has no username, go to username selection page
+                        if (userData!['username'] == null) {
+                          // Navigate to username selection page
+                          return const UsernameSelection();
+                        } else {
+                          // User has a username, show ConvoList
+                          return const ConvoList();
+                        }
                       } else {
-                        // User has a username, show ConvoList
-                        return const ConvoList();
+                        // Show nothing
+                        return const SizedBox.shrink();
                       }
-                    } else {
-                      // Show nothing
-                      return const SizedBox.shrink();
-                    }
-                  });
+                    });
+              }
+            } else {
+              // Show nothing
+              return const SizedBox.shrink();
             }
-          } else {
-            // Show nothing
-            return const SizedBox.shrink();
-          }
-        },
+          },
+        ),
+
+        //remove debug banner
+        debugShowCheckedModeBanner: false,
       ),
-      //remove debug banner
-      debugShowCheckedModeBanner: false,
     );
   }
 }
